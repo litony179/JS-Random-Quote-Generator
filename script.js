1fe5a6ac606586b9b8cd8a2cfc740183ad0322d7
Tony Li <tonyli@Tonys-MacBook-Pro.local>
2021-02-09 11:36:34 +1100

    Complete project

--- script.js
+++ script.js
@@ -1,16 +1,71 @@
-// Get Quote from API
-async function getQuote() {
-    const apiUrl = "https://type.fit/api/quotes/?method=getQuote&lang=en&format=json";
+const quoteContainer = document.querySelector('#quote-container');
+const quoteText = document.querySelector('#quote');
+const quoteAuthor = document.querySelector('#author');
+const newQuoteBtn = document.querySelector('#new-quote');
+const twitterBtn = document.querySelector('#twitter');
+const loader = document.querySelector('#loader');
+
+//Use let because we need to change the "apiQuotes"
+let apiQuotes = [];
+
+//show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function completeLoad() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+//SHow new QUote
+function newQuote() {
+    loading();
+    const singleQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    if (singleQuote.author === null) {
+        quoteAuthor.textContent = 'Unknown';
+    } else {
+        quoteAuthor.textContent = singleQuote.author;
+    }
+
+    //Check quote length
+    if (singleQuote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.add('long-quote');
+    }
+    //Set quote and hide loader
+    quoteText.textContent = singleQuote.text;
+    completeLoad();
+}
+
+// Get quotes from API
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
-
+        apiQuotes = await response.json();
+        newQuote();
     } catch (error) {
-        getQuote();
-        console.log("whoops, no quote", error);
+        alert('error detected!', error)
+            //error website
     }
 }
 
-//On Load
-getQuote();
+//Tweet a quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+    //Open a new blank tab
+    window.open(twitterUrl, '_blank');
+}
+
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//On load
+getQuotes();
